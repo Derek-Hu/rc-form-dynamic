15985ebecaaeca6db04f349f1573cc5004a3c40a
胡本绿 <benlv.hu@dianrong.com>
2020-03-06 17:53:52 +0800

    Updates

---
+++ precache-manifest.41f2eb92a76de7ee5e298b42346bf149.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d3bffddaeba0b914239bf392a74754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02c8efeae38f7bb33078",
+    "url": "/static/css/main.e86d0818.chunk.css"
+  },
+  {
+    "revision": "8eb74ac3bed48acf2da5",
+    "url": "/static/js/2.7320cb66.chunk.js"
+  },
+  {
+    "revision": "02c8efeae38f7bb33078",
+    "url": "/static/js/main.4bcb5fb0.chunk.js"
+  },
+  {
+    "revision": "16a5380971d99fde3454",
+    "url": "/static/js/runtime-main.36051816.js"
+  },
+  {
+    "revision": "05857f04ee9dcb4be9e5e90657cd6918",
+    "url": "/static/media/login.module.05857f04.less"
+  },
+  {
+    "revision": "d6ed32970195a77769b2c2f8b6accf10",
+    "url": "/static/media/search.module.d6ed3297.less"
+  }
+]);
